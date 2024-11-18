546d9c646e529f3dd3ebb798b1760e203cce2385
Manuela Ienuso <43219535+emmegray@users.noreply.github.com>
2024-11-19 00:20:48 +0100

    Update home and about

---
+++ src/components/EmmePatata.jsx
@@ -0,0 +1,3 @@
+import { motion } from "motion/react"
+
+export const EmmePatata = () => <motion.img  width="300" height="259" alt="RaccoonSpaceGif" src="/material/Emme patata.png"/>
---
+++ src/content/data.js
@@ -0,0 +1,216 @@
+export const data = {
+  Header: {
+    pages: [
+      { name: "Home", link: "/" },
+      { name: "About", link: "/about" },
+      { name: "Works", link: "/works" },
+      { name: "Posts", link: "/posts" },
+      { name: "Contact", link: "/contact" },
+    ],
+  },
+
+  About: {
+    Intro:
+      "Always been passionate about the world of art and technology, already at the age of 13 I began to self-taught the basics of digital and traditional design. I'm also a great lover of video games, comics and cinema, I especially love the horror genre and everything that has a exciting plot!",
+    Experiences: [
+      {
+        company: "Class Editori",
+        title: "Junior Web Designer",
+        subtitle: "Class Editori S.p.A. - July 2023",
+        description: "Member of the tech IT team where I developed:",
+        skills: [
+          "website development and maintenance using C#,",
+          "frontend development and web marketing campaign design,",
+          "creation and development of DEM and landing page,",
+          "management and optimization of tags and funnels and SEO optimization of digital properties."
+        ],
+      },
+      {
+        company: "Entiende",
+        title: "Web Developer",
+        subtitle: "Entiende S.r.l. - December 2022",
+        description: "Member of the tech IT team where I developed:",
+        skills: [
+          "landing pages made in React and using TypeScript,",
+          "management of the CRM (release and server) created with Angular, MongoDB and Sequelize,",
+          "building APIs and microservices with NestJS,",
+          "creation of wikis and documentation for the team with in-depth explanations on topics such as SEO and Google suites."
+        ],
+      },
+      {
+        company: "Boolean Careers",
+        title: "Junior Full Stack Web Developer Trainee",
+        subtitle: "Boolean Careers - February - September 2022",
+        description: `Full Stack Development course where I learned to:`,
+        skills: [
+          "created layout using semantic HTML5, CSS3, Bootstrap replicating web pages like Dropbox and Discord,",
+          "wrote small interfaces with Vue.js and vanilla JavaScript inspired by Netflix and Spotify,",
+          "created PHP/Laravel applications and SQL scripts,",
+          "carried out final project (webapp inspired by Airbnb) in team managing everything via Git taking care of the development of the front-end part with Vue.js"
+        ],
+      },
+      {
+        company: "Twitch",
+        title: "Twitch Streamer Affiliate",
+        subtitle: "Twitch - December 2017",
+        description: `On the platform I have:`,
+        skills: [
+          "made contents of illustration, cosplay, DIY, talk and gameplay,",
+          "work on the graphics, video and audio editing, managed moderation, communication on social media and community building,",
+          "helped me to develop my public speaking, stress management and public relations skills"
+        ],
+      },
+      {
+        company: "Ridble",
+        title: "Editor Junior",
+        subtitle: "Ridble - March - August 2015",
+        description: `I managed articles in the gaming industry and improved my post writing and news writing in real time with group editorial development and reviews of products, games and multimedia services.`,
+      },
+      {
+        company: "Artist",
+        title: "Artist",
+        subtitle: "Freelance - since 2018",
+        description: `I produced, managed and promoted my business as a commissioned graphic designer, illustrator and cosplay craftsman.`,
+      },
+    ],
+  },
+
+  Contact: {
+    Intro: "Get in Touch!",
+    Subtext: "Want to work together or have any questions?",
+    Socials: [
+      {
+        name: "Email",
+        nickname: "manuelaienuso@gmail.com",
+        link: "mailto:manuelaienuso@gmail.com",
+        icon: "fa-solid fa-envelope",
+        style:"#FFFFFF"
+      },
+      {
+        name: "GitHub",
+        nickname: "@emmegray",
+        link: "https://github.com/emmegray",
+        icon: "fa-brands fa-github",
+        style:"#FFFFFF"
+      },
+      {
+        name: "LinkedIn",
+        nickname: "",
+        link: "https://www.linkedin.com/in/manuela-ienuso-a44190111/",
+        icon: "fa-brands fa-linkedin",
+        style:"#FFFFFF"
+      },
+      {
+        name: "Twitch",
+        nickname: "@emmegray",
+        link: "https://www.twitch.tv/emmegray",
+        icon: "fa-brands fa-twitch",
+        style:"#FFFFFF"
+      },
+      {
+        name: "YouTube",
+        nickname: "@EmmeGray",
+        link: "https://www.youtube.com/@EmmeGray",
+        icon: "fa-brands fa-youtube",
+        style:"#FFFFFF"
+      },
+      {
+        name: "Instagram",
+        nickname: "@emme_gray",
+        link: "https://www.instagram.com/emme_gray",
+        icon: "fa-brands fa-instagram",
+        style:"#FFFFFF"
+      },
+      {
+        name: "DeviantArt",
+        nickname: "@emme-gray",
+        link: "https://www.deviantart.com/emme-gray",
+        icon: "fa-brands fa-deviantart",
+        style:"#FFFFFF"
+      },
+      {
+        name: "BlueSky",
+        nickname: "@emme-gray.bsky.social",
+        link: "https://bsky.app/profile/emme-gray.bsky.social",
+        icon: "fa-brands fa-bluesky",
+        style:"#FFFFFF"
+      },
+      {
+        name: "Cara",
+        nickname: "@emmegray",
+        link: "https://cara.app/emmegray",
+        icon: "fa-solid fa-c",
+        style:"#FFFFFF"
+      },
+    ]
+  },
+  
+  Works: {
+    TitleArts: "Drawing commissions:",
+    TitleEmote: "Emote commissions:",
+    Arts: [
+      {
+        alt: "Moonlight, fan art of John Martson from Red Dead Redemption for lights practice",
+        src: "works/arts/Moonlight.png",
+      },
+      {
+        alt: "Work for the MajiMonth 2020",
+        src: "works/arts/MajiMonth2020 - Hannya.png",
+      },
+      {
+        alt: "Red Dead Redemption II fan art",
+        src: "works/arts/RDR II fan art - Twitch Request.png",
+      },
+      {
+        alt: "Commission Braindamage97",
+        src: "works/arts/Commission Braindamage97 - FF7.png",
+      },
+      {
+        alt: "Commission - Xhenarjo",
+        src: "works/arts/Commission - Xhenarjo 2.png",
+      },
+      {
+        alt: "Cyberpunk",
+        src: "works/arts/ArtFight 2021 Cyberpunk OC 01.png",
+      },
+      {
+        alt: "Ash - Evil Dead doodle",
+        src: "works/arts/Ash - Evil Dead doodle.png",
+      },
+      {
+        alt: "Best way to clean John",
+        src: "works/arts/Best way to clean John.png",
+      },
+      {
+        alt: "Happy 11 Anniversary RDR",
+        src: "works/arts/Happy 11 Aniversary RDR.png",
+      },
+      {
+        alt: "Inscryption - Lonely Wizard",
+        src: "works/arts/Inscryption - Lonely Wizard.png",
+      },
+      {
+        alt: "Kofi Member reward",
+        src: "works/arts/Kofi Member reward - FEB.png",
+      },
+      {
+        alt: "Steampunk",
+        src: "works/arts/Talyn Ieiri Leo stampunk - Twitch Request.png",
+      },
+      {
+        alt: "Thomas",
+        src: "works/arts/Thomas.png",
+      },
+      {
+        alt: "Xhenarjo drawing",
+        src: "works/arts/Xhenarjo drawing 01 credit.png",
+      },
+      {
+        alt: "Zhao",
+        src: "works/arts/Zhao.png",
+      },
+    ]
+  },
+};
+
+
