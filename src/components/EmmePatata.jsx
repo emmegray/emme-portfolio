e329b4cb1fb93701c33de0fdf50d2ee93fc6620a
emmegray <lunaienuso@libero.it>
2025-09-11 17:19:39 +0200

    Fix typo

--- src/components/EmmePatata.jsx
+++ src/components/EmmePatata.jsx
@@ -1,3 +1,3 @@
 import { motion } from "motion/react"
 
-export const EmmePatata = () => <motion.img  width="300" height="259" alt="RaccoonSpaceGif" src="/material/Emme patata.png"/>
+export const EmmePatata = () => <motion.img  width="300" height="259" alt="Emme Patata" src="/material/Emme patata.png"/>
